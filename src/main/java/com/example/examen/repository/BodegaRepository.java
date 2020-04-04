c6a6739fa4224b6fa308289d31669a0883e8b7a3
GABOCALVO\Gabriel <gabriel.calvo.vargas@gmail.com>
2020-04-04 14:42:33 -0600

    Repository annotations corregidas

--- src/main/java/com/example/examen/repository/BodegaRepository.java
+++ src/main/java/com/example/examen/repository/BodegaRepository.java
@@ -1,9 +1,11 @@
 package com.example.examen.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.examen.domain.Bodega;
 
+@Repository
 public interface BodegaRepository extends JpaRepository<Bodega, String>{
 
 }
--- src/main/java/com/example/examen/repository/SuministroRepository.java
+++ src/main/java/com/example/examen/repository/SuministroRepository.java
@@ -1,9 +1,11 @@
 package com.example.examen.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.examen.domain.Suministro;
 
+@Repository
 public interface SuministroRepository extends JpaRepository<Suministro, Integer>{
 
 }
